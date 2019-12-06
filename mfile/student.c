1d114eb28cfda7d8f26da18cec94bdf67c6c3ee8
Darius <yazdekhastim9687@my.sduhsd.net>
2019-12-06 05:51:22 -0800

    Final additions

--- mfile/student.c
+++ mfile/student.c
@@ -3,5 +3,5 @@
 
 void printStudent(struct Student* student)
 {
-	printf("The student's name is %s %s\nThe student's age is %d\nThe student's ID number is %d\n", student->firstname, student->lastname, student->age, student->studentid);
+	printf("The student's name is %s %s\nThe student's age is %d\nThe student's ID number is %d\n", student->firstName, student->lastName, student->age, student->studentid);
 }
