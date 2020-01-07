12eb023c40adb33b3b65719a38155a3e3cdba573
Darius Y <yazdekhastim9687@my.sduhsd.net>
2020-01-07 13:09:09 -0800

    New edits to circle code

--- areaOfCircle.c
+++ areaOfCircle.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
+
+  float radius;
+  float PI = 3.14159265;
+
+  for (int i = 3.5; i <= 12.5; i++)
+  {
+	radius = i;
+	float area = PI *radius*radius;
+	printf("The area of the circle is %f\n", area);
+	
+  }
+
+
+
+
 }
