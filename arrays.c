714751df3226f583d1cfa20d4ed5b790a3698abf
Darius Y <yazdekhastim9687@my.sduhsd.net>
2020-01-09 13:19:50 -0800

    New makefile added

--- arrays.c
+++ arrays.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+// This is the function that adds 1 to the index
 int arrayAdd(int* arr, int s, int n)
 {
 	for (int i = 0; i < s; i++)
@@ -11,10 +11,13 @@ int arrayAdd(int* arr, int s, int n)
 
 int main()
 {
+	// Initializes the array
 	int arr[100];
+	// For loop that multiplies the indexes
 	for (int i = 0; i < 100; i++)
 	{
 		arr[i] = i * i;
 	}
 	arrayAdd(arr, 100, 1);
 }
+
