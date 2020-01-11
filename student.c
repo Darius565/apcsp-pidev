1bef61bb47f77921fdbf33d1b5253c350848dd51
Darius565 <58751093+Darius565@users.noreply.github.com>
2020-01-10 23:45:31 -0800

    Update student.c

--- student.c
+++ student.c
@@ -9,9 +9,7 @@ struct Student {
 };
 void printStudent(struct Student* student)
 {
-  printf("The student's name is %s %s\n", student->firstname, student->lastname);
-	printf("The student's age is %d\n", student->age);
-	printf("The student's ID number is %d\n", student->studentid);
+	printf("The student's name is %s %s\nThe student's age is %d\nThe student's ID number is %d\n", student->firstname, student->lastname, student->age, student->studentid);
 }
 
 int main()
