436b07e4df13495d59722e02f7025008d96d5ce0
Darius565 <58751093+Darius565@users.noreply.github.com>
2020-01-10 22:43:10 -0800

    Update student.c

--- student.c
+++ student.c
@@ -13,31 +13,32 @@ void printStudent(struct Student* student)
 	printf("The student's age is %d\n", student->age);
 	printf("The student's ID number is %d\n", student->studentid);
 }
+
 int main()
 {
 	char input[50];
-	int answer;
-	printf("How many students are you entering?\n");
+	int howmanystudents;
+	printf("How many students do you want?\n");
 	fgets(input, 256, stdin);
-	sscanf(input, "%d", &answer);
-	struct Student studentA[answer];
-	for (int i = 0; i<answer; i++)
+	sscanf(input, "%d", &howmanystudents);
+	struct Student studentA[howmanystudents];
+	for (int i = 0; i < howmanystudents; i++)
 	{
-	printf("What is student's first name?\n");
-	fgets(input, 256,stdin);
-	sscanf(input, "%s", &studentA[i].firstname);
-	printf("What is student's last name?\n");
-	fgets(input, 256, stdin);
-	sscanf(input, "%s", &studentA[i].lastname);
-	printf("What is student's age?\n");
-	fgets(input, 256, stdin);
-	sscanf(input, "%d", &studentA[i].age);
-	printf("What is student's ID number\n");
-	fgets(input, 256, stdin);
-	sscanf(input, "%d", &studentA[i].studentid);
+		printf("What is student's first name?\n");
+		fgets(input, 256,stdin);
+		sscanf(input, "%s", &studentA[i].firstName);
+		printf("What is student's name?\n");
+		fgets(input, 256, stdin);
+		sscanf(input, "%s", &studentA[i].lastName);
+		printf("What is student's age?\n");
+		fgets(input, 256, stdin);
+		sscanf(input, "%d", &studentA[i].age);
+		printf("What is student's ID number\n");
+		fgets(input, 256, stdin);
+		sscanf(input, "%d", &studentA[i].studentid);
 	}
-	for (int i = 0; i<answer; i++)
+	for (int i = 0; i < howmanystudents; i++)
 	{
-	printStudent(&studentA[i]);
+		printStudent(&studentA[i]);
 	}
 }
